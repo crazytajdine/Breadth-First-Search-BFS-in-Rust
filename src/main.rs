411941b17a3e6d86ba84ba3ceb1b870cb63e3c2d
Taj Eddine Marmoul <tajdinetajdine1@gmail.com>
2025-05-25 21:05:10 +0100

    bfs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
-use math::find_k;
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt::Display,
+    hash::Hash,
+};
 
-mod math;
+type Graph<T> = HashMap<T, Vec<T>>;
+
+fn bfs<'a, T: Eq + Hash + Display>(tree: &'a Graph<T>, start: &'a T) -> Vec<&'a T> {
+    let mut visited = Vec::<&'a T>::new();
+
+    let mut queue = VecDeque::<&T>::new();
+
+    queue.push_back(start);
+
+    while let Some(step) = queue.pop_front() {
+        visited.push(step);
+
+        if let Some(dests) = tree.get(step) {
+            let nextdests: Vec<&T> = dests
+                .iter()
+                .filter(|dest| !visited.contains(dest) && !queue.contains(dest))
+                .collect();
+
+            for dest in nextdests {
+                queue.push_back(dest);
+            }
+        }
+    }
+
+    visited
+}
 
 fn main() {
-    let r = ["da", "d"];
+    let mut v = Graph::new();
 
-    if let Some(res) = find_k(&r, &"da") {
-        println!("{}", res);
+    v.insert(1, vec![2, 3]);
+    v.insert(2, vec![3]);
+    v.insert(3, vec![5]);
+    v.insert(4, vec![3]);
+
+    let visited = bfs(&v, &1);
+
+    for vis in visited {
+        print!("{},", vis)
     }
+    println!()
 }
